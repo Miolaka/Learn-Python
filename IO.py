a0e703704ce8934d4111f70979ce2b46b98c2c1a
Miolaka <Miolaka@outlook.com>
2021-03-27 05:50:21 +0100

    update

---
+++ IO.py
@@ -0,0 +1,16 @@
+import urllib
+import os
+
+x=input('some number:')
+y=raw_input('anything:') #raw_input is not defined.
+
+file=open('./stuff.dat', 'wa')
+file.write(y)
+file.close()
+
+os.remove('./stuff.dat')
+
+file=urllib.urlopen("http://www.google.com")
+cotent=file.readlines()
+
+print content
